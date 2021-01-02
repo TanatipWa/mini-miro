f60642e0c024b38cc08c771b4593a90b58a9a177
Tanatip Waleeroeksap <68233192+TanatipWa@users.noreply.github.com>
2021-01-02 10:34:21 +0700

    Delete db.js

--- utl/db.js
+++
@@ -1,8 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/database";
-
-export const db = firebase
-  .initializeApp({ databaseURL: "https://mini-miro-28f08.firebaseio.com/" })
-  .database();
-
-// "https://mini-miro.firebaseio.com/"
